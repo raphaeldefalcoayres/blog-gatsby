5be28eee84e022d8225feab6602207e62e99df9c
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2020-04-30 11:59:47 -0300

    add eslint and configs and style guide airbnb and etc

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,17 +5,15 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Layout = ({ children }) => {
-  return (
-    <main>{children}</main>
-  )
-}
+const Layout = ({ children }) => (
+  <main>{children}</main>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
