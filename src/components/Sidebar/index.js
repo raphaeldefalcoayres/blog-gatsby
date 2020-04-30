b7c6b81df13f9471a9101bf6cf11b478df11b454
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2020-04-30 12:03:56 -0300

    update files to new configs and change style path layout to Layout/index and initial create Sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sidebar = () => (
+  <sidebar>
+    <h1>Raphael</h1>
+    <h2>
+      Compartilhando conteúdos sobre
+      <br />
+      Desenvolvimento e Inovação
+    </h2>
+  </sidebar>
+);
+
+export default Sidebar;
